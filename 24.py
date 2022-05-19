cd18f9a2e2794e0262109db62f1c688b638a97ca
AnderP-cod <nemo001leyn@gmail.com>
2022-05-19 19:27:02 +0200

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,30 +1,35 @@
-from sys import stdin
 
-class Bankacaynt():
+"""from sys import stdin"""
 
-    def __init__(self,room,pincot,pye):
+
+# класс пользовательский акаунт
+class Bankacaynt:
+    def __init__(self, room, pincot, pye):
         self.__room = room
         self.__pincot = pincot
         self.__pye = pye
 
-    def __add__(self):
+    def __add__(self):  # ввод данных и проверка их
+        nu1 = 10000000000000000
+        nu2 = 999999999999999
         self.__room = int(input("Напишите номер карты: "))
-        if self.__room >= 999999999999999 and self.__room <= 10000000000000000: #1234567891234567
+        if self.__room >= nu2 and self.__room <= nu1:
             self.__pincot = int(int(input("Напишите пинкот: ")))
             if self.__pincot >= 1000 and self.__pincot < 10000:
-                self.__pye = int(input("Напишите сколько денег у вас на карте: "))
+                self.__pye = int(input("Напишите сколько денег /"
+                                       "у вас на карте: "))
                 return "Вы вошли в акаунт банка"
         else:
             print("Ошибка")
 
-
-    def All_information(self):
+    def All_information(self):  # вся информация про акаунт пользователя
         pincot = input("Pin: ")
         if self.__pincot == int(pincot):
-            return f"Карта {self.__room}, пинкот {self.__pincot},  баланс {self.__pye}"
+            return f"Карта {self.__room}, пинкот" \
+                   f" {self.__pincot},  баланс {self.__pye}"
         return "Неправельный pin"
 
-    def Top_up_card(self):
+    def Top_up_card(self):  # функция которая позволяет ложить деньги на карту
         pincot = input("Pin: ")
         if self.__pincot == int(pincot):
             pye = input("На какую сумму хотите пополнить карточку: ")
@@ -32,34 +37,40 @@ class Bankacaynt():
             return f"{self.__pye}"
         return "Неправельный pin"
 
-    def Xiang_money(self):
+    def Xiang_money(self):  # съем денег с карты
         pincot = input("Pin: ")
         if self.__pincot == int(pincot):
             pye2 = int(input("Сколько денег вы хотите снять денег с карты: "))
             if pye2 <= self.__pye:
                 self.__pye -= int(pye2)
-                return f'Вы сняли  {pye2} грн \nу вас осталося на карте {self.__pye} '
+                return f'Вы сняли  {pye2} грн \n' \
+                       f'у вас осталося на карте {self.__pye} '
             else:
                 return "Вы хотите снять больше чем у вас на карте"
         else:
             return "Неправельный pin"
 
 
-
+# бонкомат наследуеться от банкакаунт
 class Bankomat(Bankacaynt):
+    """Спомощью этого класса вызываються все функции с банкакаунта"""
     def __init__(self):
         super().__init__()
+
     @staticmethod
     def Bankomat_Balance_check():
         return emp_1.All_information()
+
     @staticmethod
     def Bankomat_Top_up_card():
         return emp_1.Top_up_card()
+
     @staticmethod
     def Bankomat_Xiang_money():
         return emp_1.Xiang_money()
 
-emp_1 = Bankacaynt("","","")
+
+emp_1 = Bankacaynt('', '', '')
 print(emp_1.__add__())
 print()
 print("1) Посмотреть баланс")
